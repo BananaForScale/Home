e9dcb74f2221acbccacd5e67b5e6dcc6c4ee2057
Henry Slawniak <meggawatts@gmail.com>
2014-05-14 16:46:40 -0500

    Tile 0,-1

--- map/mappoints.js
+++ map/mappoints.js
@@ -2,5 +2,9 @@ var MAP_POIs = [
     {'name': 'Port Sebring', 'x': -0.02, 'y': 0.39 },
     {'name': 'origin', 'x': 0.00, 'y': 0.00 },
     {'name': 'Acton', 'x': -0.06, 'y': -0.08 },
-    {'name': 'Shackleton', 'x': 0.15, 'y': -0.44 },
+    {'name': 'Shackleton', 'x': 0.15, 'y': -0.58 },
+    {'name': 'Franksburg', 'x': 0.22, 'y': -0.44 },
+    {'name': 'Edgeworth', 'x': 0.46, 'y': -0.92 },
+    {'name': 'Point Adams', 'x': -0.10, 'y': -1.14 },
+    {'name': 'Marshalltoen', 'x': 0.10, 'y': -1.45 },
 ]
