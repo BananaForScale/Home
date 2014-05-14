055af351b91c83e6d3689bf2d259893ed22e7c38
Henry Slawniak <meggawatts@gmail.com>
2014-05-14 16:27:58 -0500

    Update map points

--- map/mappoints.js
+++ map/mappoints.js
@@ -1,4 +1,6 @@
 var MAP_POIs = [
-    {'name': 'Port Sebring', 'x': 0.00, 'y': 0.35 },
+    {'name': 'Port Sebring', 'x': -0.02, 'y': 0.39 },
     {'name': 'origin', 'x': 0.00, 'y': 0.00 },
+    {'name': 'Acton', 'x': -0.06, 'y': -0.08 },
+    {'name': 'Shackleton', 'x': 0.15, 'y': -0.44 },
 ]
